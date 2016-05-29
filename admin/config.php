ba8a5a023f0c264a4c174ff62e1d46ff3b33940a
jgao <jgao@i9i8.com>
2016-05-29 15:59:27 +0800

    fix project root bug

--- admin/config.php
+++ admin/config.php
@@ -7,19 +7,22 @@ define('HTTP_CATALOG', 'http://buy.do4i.com/');
 define('HTTPS_SERVER', 'http://buy.do4i.com/admin/');
 define('HTTPS_CATALOG', 'http://buy.do4i.com/');
 
+//project root
+define('PRO_ROOT','/Users/jgao/gitpro/zing/');
+
 // DIR
-define('DIR_APPLICATION', '/Users/jgao/gitpro/zing/admin/');
-define('DIR_SYSTEM', '/Users/jgao/gitpro/zing/system/');
-define('DIR_IMAGE', '/Users/jgao/gitpro/zing/image/');
-define('DIR_LANGUAGE', '/Users/jgao/gitpro/zing/admin/language/');
-define('DIR_TEMPLATE', '/Users/jgao/gitpro/zing/admin/view/template/');
-define('DIR_CONFIG', '/Users/jgao/gitpro/zing/system/config/');
-define('DIR_CACHE', '/Users/jgao/gitpro/zing/system/storage/cache/');
-define('DIR_DOWNLOAD', '/Users/jgao/gitpro/zing/system/storage/download/');
-define('DIR_LOGS', '/Users/jgao/gitpro/zing/system/storage/logs/');
-define('DIR_MODIFICATION', '/Users/jgao/gitpro/zing/system/storage/modification/');
-define('DIR_UPLOAD', '/Users/jgao/gitpro/zing/system/storage/upload/');
-define('DIR_CATALOG', '/Users/jgao/gitpro/zing/catalog/');
+define('DIR_APPLICATION', PRO_ROOT . 'admin/');
+define('DIR_SYSTEM', PRO_ROOT . 'system/');
+define('DIR_IMAGE', PRO_ROOT . 'image/');
+define('DIR_LANGUAGE', PRO_ROOT . 'admin/language/');
+define('DIR_TEMPLATE', PRO_ROOT . 'admin/view/template/');
+define('DIR_CONFIG', PRO_ROOT . 'system/config/');
+define('DIR_CACHE', PRO_ROOT . 'system/storage/cache/');
+define('DIR_DOWNLOAD', PRO_ROOT . 'system/storage/download/');
+define('DIR_LOGS', PRO_ROOT . 'system/storage/logs/');
+define('DIR_MODIFICATION', PRO_ROOT . 'system/storage/modification/');
+define('DIR_UPLOAD', PRO_ROOT . 'system/storage/upload/');
+define('DIR_CATALOG', PRO_ROOT . 'catalog/');
 
 // DB
 define('DB_DRIVER', 'mpdo');
